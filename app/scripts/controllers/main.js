97bacfaf8ad1fd3aa11a1fb380628723b0a9a697
Annie Chen <annieche@andrew.cmu.edu>
2015-05-28 16:36:28 -0700

    Added snooze buttons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -17,17 +17,18 @@ angular.module('yoTestApp')
     function init(){
 	    $scope.todoItems = [];
 	    $scope.todosCompleted = 0;
+	    $scope.snoozeClicked = false;
 	    initNewItem();
     }
-    
     function initNewItem(){
-    	$scope.newItem = {'item': '', 'completed': false};
+    	$scope.newItem = {'item': '', 'completed': false, 'snoozeTime': ''};
     }
+    //todo actions
     $scope.addItem = function(){
     	if ($scope.newItem.item !== ''){
 	    	$scope.todoItems.push($scope.newItem);
 	    	initNewItem();
-    	};
+    	}
     };
     $scope.completeTodo = function(todo){
     	todo.completed = true;
@@ -36,11 +37,20 @@ angular.module('yoTestApp')
     $scope.deleteTodo = function(todoIndex){
     	delete $scope.todoItems[todoIndex];
     };
+    $scope.snoozeTodo = function(todoIndex, time){
+    	$scope.todoItems[todoIndex].snoozeTime = time;
+    	console.log($scope.todoItems[todoIndex].snoozeTime);
+    	$scope.snoozeClicked = false;
+    };
+    //filters
     $scope.filterCompleted = function(item){
     	return item.completed === true;
     };
     $scope.filterIncomplete = function(item){
-    	return item.completed === false;
+    	return item.completed === false && item.snoozeTime === '';
+    };
+    $scope.filterSnooze = function(item){
+    	return item.snoozeTime !== '';
     };
     init();
   });
